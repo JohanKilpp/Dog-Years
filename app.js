a3f538e57fac2461fd8f89e611f25440ec90276a
JohanKilpp <50911484+JohanKilpp@users.noreply.github.com>
2020-04-28 05:13:34 +1000

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// set you age
+let myAge = 35;
+// define the early years
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+//subtract the first 2 years from your age to calculate the remaning
+let laterYears = (myAge - 2) * 4;
+
+//console.log(`Early Years: ${earlyYears}, Later years: ${laterYears}.` );
+//your age in Dog years
+let myAgeInDogYears = earlyYears + laterYears;
+//def the name in lower case
+let myName = 'Johan Peter Kilpp'.toLowerCase()
+//result
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.` );
+
